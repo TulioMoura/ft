a385b35995ad6d9b042cc1146d5d89bec39cbad4
Túlio <90994955+TulioMoura@users.noreply.github.com>
2023-04-04 23:33:38 -0300

    add scale to speed gauge

--- velocity.js
+++ velocity.js
@@ -1,13 +1,20 @@
-velocity_gauge = document.getElementById("velocity")
-instant_speed = Number(velocity_gauge.getElementsByTagName("h1")[0].innerHTML)
-console.log(instant_speed)
+function updateSpeedGauge(speed)
+{velocity_gauge = document.getElementById("velocity")
+velocity_gauge.getElementsByTagName("h1")[0].innerHTML = speed 
+
 velocity_range = 240 //max speed
-step = 90/80
-turn_angle = step * instant_speed/3
+if(speed > velocity_range){
+    speed  = velocity_range;
+}
+step = 90/80 //size of each step image
+turn_angle = step * speed/3 //angle to turn in each image to get expected fill of bar
+
 velocity_gauge_bar = velocity_gauge.getElementsByClassName("move")
 for(let i =1;i<=3;i++){
     console.log(velocity_gauge_bar[i-1])
 velocity_gauge_bar[i-1].setAttribute("style",`transform: rotateZ(${turn_angle*i}deg);`)
 console.log(turn_angle*i)
 }
+}
 
+updateSpeedGauge(152)
