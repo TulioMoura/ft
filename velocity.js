bb6edec000ac6f48f4a298cac8ed293bb7586886
Túlio <90994955+TulioMoura@users.noreply.github.com>
2023-04-06 02:02:21 -0300

    added timing fields

--- velocity.js
+++ velocity.js
@@ -1,28 +1,58 @@
-function updateSpeedGauge(speed)
-{velocity_gauge = document.getElementById("velocity")
-velocity_gauge.getElementsByTagName("h1")[0].innerHTML = speed 
+function updateSpeedGauge(speed) {
+    if (typeof (speed) != 'number') {
+        return ("Invalid parameter Value")
+    } velocity_gauge = document.getElementById("velocity")
+    velocity_gauge.getElementsByTagName("h1")[0].innerText = speed
 
-velocity_range = 240 //max speed
-if(speed > velocity_range){
-    speed  = velocity_range;
-}
-step = 90/80 //size of each step image
-turn_angle = step * speed/3 //angle to turn in each image to get expected fill of bar
+    velocity_range = 240 //max speed
+    if (speed > velocity_range) {
+        speed = velocity_range;
+    }
+    step = 90 / 80 //size of each step image
+    turn_angle = step * speed / 3 //angle to turn in each image to get expected fill of bar
 
-velocity_gauge_bar = velocity_gauge.getElementsByClassName("move")
-for(let i =1;i<=3;i++){
-    console.log(velocity_gauge_bar[i-1])
-velocity_gauge_bar[i-1].setAttribute("style",`transform: rotateZ(${turn_angle*i}deg);`)
-console.log(turn_angle*i)
-}
+    velocity_gauge_bar = velocity_gauge.getElementsByClassName("move")
+    for (let i = 1; i <= 3; i++) {
+        velocity_gauge_bar[i - 1].setAttribute("style", `transform: rotateZ(${turn_angle * i}deg);`)
+    }
 }
 
-function updateRpmBar(rpm){
-    barPercent = 99/10000 * rpm
+function updateRpmBar(rpm) {
+    if (typeof (rpm) != 'number') {
+        return ("Invalid parameter Value")
+    }
+    barPercent = 99 / 10000 * rpm
     rpmValue = document.getElementById("rpmcounter").getElementsByTagName("h1")[0]
-    rpmValue.innerHTML = rpm
-    document.getElementById("shiftbar").setAttribute("style",`width:${barPercent}%;`)
+    rpmValue.innerText = rpm
+    document.getElementById("shiftbar").setAttribute("style", `width:${barPercent}%;`)
 }
 
+function updateTpsBar(tps) {
+    if (typeof (tps) != 'number') {
+        return ("Invalid parameter Value")
+    }
+    barPercent = 96 / 100 * tps
+    tpsmeter = document.getElementById("tpsmeter")
+    tpsmeter.setAttribute("style", `height:${barPercent}%;`)
+    tpsValueField = document.getElementById("tpsBarPercent")
+    tpsValueField.innerText = tps;
+}
+
+
+function tofullthrottle(){
+    tpsValueField = document.getElementById("tpsBarPercent")
+    tps = Number( tpsValueField.innerText)
+    tps++;
+    if(tps>100){
+        tps = 0;
+    }
+    console.log("running" + tps)
+    console.log(updateTpsBar(tps))
+    
+}
+
+//setInterval(tofullthrottle,50)
+
+updateTpsBar(75)
 updateRpmBar(10000)
 updateSpeedGauge(152)
